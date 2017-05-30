4dbb0a2700189bd4ef3f2cf4a568bee6c74202f9
evgeniy.kulikov <evgeniy.kulikov@ainstainer.de>
2017-05-30 14:36:09 +0300

    utf-8 encoding was added

--- e2e-typescript-testsuit/chooseBrowserAndFeature.ps1
+++ e2e-typescript-testsuit/chooseBrowserAndFeature.ps1
@@ -1,4 +1,7 @@
 param($browser, $feature) 
 
-cat config/config-template.ts | % { $_ -replace "browserPlaceholder", "$browser" } | % { $_ -replace "featurePlaceHolder", "$feature" } > config/config.ts
+cat config/config-template.ts | % { $_ -replace "browserPlaceholder", "$browser" } | % { $_ -replace "featurePlaceHolder", "$feature" } | Set-Content -Encoding UTF8 -Path "config/config.ts"
+
+
+
 
